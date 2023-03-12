a1c40e98bd0b51d2a22d3f0823b4e03083420e3b
Ben Sales <adebsa2401@gmail.com>
2023-03-12 13:30:15 +0100

    feat: Implement user's authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_signup_params, only: [:create]
+
+  protected
+
+  def configure_signup_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
